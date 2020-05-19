2caf8d0772845e839cb98a7973bfd408117485e5
Strohlaj <Aaron.Strohl@outlook.com>
2020-05-18 21:55:10 -0400

    tabs or spaces? - spaces i suppose.

--- Frame.lua
+++ Frame.lua
@@ -12,13 +12,13 @@ warriorAuditTable = {};
 SLASH_SUNDERER1, SLASH_SUNDERER2 = '/sun', '/sunderer';
 
 function SlashCmdList.SUNDERER(msg, ...)
-	if (msg == 'show') then
-		Frame1:Show();
-	elseif (msg == 'hide') then
-		Frame1:Hide();
-	else 
-		print ("Sunderer: Arguments to /sun or /sunderer: \n Hide - Hide the Sunderer frame. \n Show - Show the Sunderer frame.");
-	end
+    if (msg == 'show') then
+        Frame1:Show();
+    elseif (msg == 'hide') then
+        Frame1:Hide();
+    else 
+        print ("Sunderer: Arguments to /sun or /sunderer: \n Hide - Hide the Sunderer frame. \n Show - Show the Sunderer frame.");
+    end
 end
 
 function beginTrackingSunderers()
@@ -31,63 +31,62 @@ function beginTrackingSunderers()
 	
     getWarriorsInRaid();
     clearSundererList();
-	fillSundererList();
-	
+    fillSundererList();
 end
 
 function clearSundererList()
-	sundererOne:SetText("");
-	sundererTwo:SetText("");
-	sundererThree:SetText("");
-	sundererFour:SetText("");
-	sundererFive:SetText("");
-	
-	sundererChkBtnOne:SetChecked(false);
-	sundererChkBtnTwo:SetChecked(false);
-	sundererChkBtnThree:SetChecked(false);
-	sundererChkBtnFour:SetChecked(false);
-	sundererChkBtnFive:SetChecked(false);
+    sundererOne:SetText("");
+    sundererTwo:SetText("");
+    sundererThree:SetText("");
+    sundererFour:SetText("");
+    sundererFive:SetText("");
+    
+    sundererChkBtnOne:SetChecked(false);
+    sundererChkBtnTwo:SetChecked(false);
+    sundererChkBtnThree:SetChecked(false);
+    sundererChkBtnFour:SetChecked(false);
+    sundererChkBtnFive:SetChecked(false);
 end
 
 function fillSundererList()
-	setSundererText(sundererOne, sundererChkBtnOne);
-	setSundererText(sundererTwo, sundererChkBtnTwo);
-	setSundererText(sundererThree, sundererChkBtnThree);
-	setSundererText(sundererFour, sundererChkBtnFour);
-	setSundererText(sundererFive, sundererChkBtnFive);
+    setSundererText(sundererOne, sundererChkBtnOne);
+    setSundererText(sundererTwo, sundererChkBtnTwo);
+    setSundererText(sundererThree, sundererChkBtnThree);
+    setSundererText(sundererFour, sundererChkBtnFour);
+    setSundererText(sundererFive, sundererChkBtnFive);
 end
 
 -- Populates the fontstring and pairs the checkbutton with the randomly selected warrior table.
 function setSundererText(fontString, chkBtn)
-	local num = math.random(1, warriorTableCount);
-	local playerName = warriorTable[num];
-	warriorAuditTable[playerName] = chkBtn;
+    local num = math.random(1, warriorTableCount);
+    local playerName = warriorTable[num];
+    warriorAuditTable[playerName] = chkBtn;
     fontString:SetText(playerName);
-	SendChatMessage("!!---> !Sunder Boss! <---!!", "WHISPER", nil, GetUnitName(playerName))
-	table.remove(warriorTable, num);
-	warriorTableCount = warriorTableCount - 1;
+    SendChatMessage("!!---> !Sunder Boss! <---!!", "WHISPER", nil, GetUnitName(playerName))
+    table.remove(warriorTable, num);
+    warriorTableCount = warriorTableCount - 1;
 end
 
 -- Modifies the warriorTable variable and warriorTableCount
 function getWarriorsInRaid()
-
-	for i = 40, 1, -1 do
-		name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
-		if (name ~= nil and class == "Warrior" and role ~= "MAINTANK" and online) then
-			print(name);
-			print(role);
-			table.insert(warriorTable, name);
-			warriorTableCount = warriorTableCount + 1;
-		end
+    for i = 40, 1, -1 do
+        name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i);
+        if (name ~= nil and class == "Warrior" and role ~= "MAINTANK" and online) then
+            print(name);
+            print(role);
+            table.insert(warriorTable, name);
+            warriorTableCount = warriorTableCount + 1;
+        end
 	end
-	for i,v in ipairs(warriorTable) do print(i,v) end
+	-- For debug - keep in here because I don't want to look this ipairs func up again.
+    -- for i,v in ipairs(warriorTable) do print(i,v) end
 end
 -- Monitors combat log to identify if any of the chosen sunderers performed their duty - if they do, checkbox is selected.
 function Combat_OnEvent(self, event, ...)
-	if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
-		local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool = CombatLogGetCurrentEventInfo();
-		if (spellName == "Sunder Armor" and subevent == "SPELL_CAST_SUCCESS" and warriorAuditTable[sourceName] ~= nil) then
-			warriorAuditTable[sourceName]:SetChecked(true);
-		end
-	end
+    if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
+        local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool = CombatLogGetCurrentEventInfo();
+        if (spellName == "Sunder Armor" and subevent == "SPELL_CAST_SUCCESS" and warriorAuditTable[sourceName] ~= nil) then
+            warriorAuditTable[sourceName]:SetChecked(true);
+        end
+    end
 end
